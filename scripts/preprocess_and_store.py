e9445536f13040f65122cd3764f523576c149069
popkristina <pop.kristina1@yandex.com>
2024-09-09 18:14:49 +0000

    add more code logic

--- scripts/preprocess_and_store.py
+++ scripts/preprocess_and_store.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import argparse
+
+
+###################### ARGUMENT PARSER #######################
+
+parser = argparse.ArgumentParser(description='Inputs for script.')
+parser.add_argument('--new_data', dest='new_docs_path',
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+
 
 ######## READ NEW DATA TO UPDATE VECTOR DATABASE WITH ########
 
