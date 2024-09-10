8c331bd50ca29065f3491bd8be8902c5bca0fdcc
popkristina <pop.kristina1@yandex.com>
2024-09-10 13:23:49 +0000

    preprocess store update

--- scripts/preprocess_and_store.py
+++ scripts/preprocess_and_store.py
@@ -1,62 +1,84 @@
-# Note: This script is intended to show how the process would
-# look like and is not indended to run at this point
+"""
+v01: Initial concept of the process
+
+In case we want to update the knowledge base with fresh
+documents, we give a path to where the new documents are.
+
+We also give a path to where we want to store the
+preprocessed and chunked files.
+
+In the POC, these files are stored in our project file
+system as we have no restrictions on the public data usage.
+
+"""
 
 import pandas as pd
 import argparse
 import boto3
 import json
+import os
+import glob
 from utils import *
-from io import StringIO  
-
-###################### ARGUMENT PARSER #######################
+from io import StringIO
 
 
-"""
-In case we want to update the knowledge base with fresh documents,
-we give a path to where the new documents are. 
-We also give a path to where we want to store  the preprocessed
-and chunked files.
-"""
-parser = argparse.ArgumentParser()
-parser.add_argument("--input-data", type=str, dest='data_update', 
-                    help='data to update the knowledge base with')
-parser.add_argument('--output', type=str, dest='prepped_data')
-args = parser.parse_args()
+#  DEFINE GLOBAL VARIABLES #########################################
 
 
-######## READ NEW DATA TO UPDATE VECTOR DATABASE WITH #########
-
-new_files_path = args.data_update
-
-# TODO: Implement additional file format checks in case 
-# not all documents are .md format
-md_files = glob.glob(os.path.join(new_files_path, '*.md'))
-df = read_glob_files(md_files)
-
-# Upload data to S3 bucket 
-# Not tested if this will work
-
-csv_buffer = StringIO()
-df.to_csv(csv_buffer, index=False)
-
+# Necessary vars for s3 bucket connection
 s3_resource = boto3.resource('s3')
-s3_resource.Object(
-    'sagemaker-studio-863518450685-ozz0puftsu9', 
-    'data/new_raw_documents/documents.csv').put(Body=csv_buffer.getvalue())
+bucket_name = 'sagemaker-studio-863518450685-ozz0puftsu9'
 
-
-################ TURN NEW DATA INTO VECTORS ##################
-
-#TODO: Actually deploy model to make this piece of code work
+# Abritrary that would be created within bucket
+preprocessed_path_s3 = 'data/new_raw_documents/documents.csv'
 
 # Initialize the SageMaker runtime client
 sagemaker_runtime = boto3.client('runtime.sagemaker')
 
 
+# ARGUMENT PARSER #################################################
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input-data", type=str, dest='data_update',
+                    help='data to update the knowledge base with')
+parser.add_argument('--output', type=str, dest='prepped_data')
+args = parser.parse_args()
+
+
+# READ NEW DATA TO UPDATE VECTOR DATABASE WITH ###################
+
+
+new_files_path = args.data_update
+
+# TODO: Implement additional file format checks in case
+#       not all documents are .md format
+
+# TODO: Update to read from alternative sources according
+#       to business demand
+
+md_files = glob.glob(os.path.join(new_files_path, '*.md'))
+df = read_glob_files(md_files)
+
+# Uploads data to S3 bucket (Not tested if this will work)
+csv_buffer = StringIO()
+df.to_csv(csv_buffer, index=False)
+
+s3_resource.Object(
+    bucket_name, preprocessed_path_s3
+).put(Body=csv_buffer.getvalue())
+
+
+# TURN NEW DATA INTO VECTORS ##################################
+
+
+# TODO: Actually deploy model to make this piece of code work
+
+
 def get_embeddings(text):
     # TODO: Move this function to utils with all
     # other functionalities
-    
+
     response = sagemaker_runtime.invoke_endpoint(
         EndpointName='your-endpoint-name',
         ContentType='application/json',
